8495abfb76e0b1d498bc73828df08ffbcb8cd2fc
Jonas <ondo.inbox@gmail.com>
2025-11-28 12:25:58 +0100

    StartScreen

---
+++ src/screens/StartScreen/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',  
+    alignItems: 'center',
+    backgroundColor: '#fff',    
+  },
+  image: {
+    width: 250,
+    height: 250,
+    resizeMode: 'contain',
+  },
+});
